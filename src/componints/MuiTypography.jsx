b94f87aa7380f6cbbbf86d2b334fd9ea165666e7
Mohammad Ashif <Mohammad Ashif>
2024-01-21 21:27:12 +0600

    mui installed

---
+++ src/componints/MuiTypography.jsx
@@ -0,0 +1,21 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant='h1'>h1 Heading</Typography>
+            <Typography variant='h2'>h2 Heading</Typography>
+            <Typography variant='h3'>h3 Heading</Typography>
+            <Typography variant='h4'>h4 Heading</Typography>
+            <Typography variant='h5'>h5 Heading</Typography>
+            <Typography variant='h5'>h5 Heading</Typography>
+            <Typography variant='subtitle1'>Sub title 1</Typography>
+            <Typography variant='subtitle2'>Sub title 2</Typography>
+            <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptate vitae sit quia exercitationem laboriosam culpa officia ipsa? Aut quos assumenda rerum, natus nam iste doloribus molestias. Autem, nam fugit.</Typography>
+            <Typography variant='body2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda repudiandae tempora quae, dicta nulla aliquam blanditiis voluptatum hic error, debitis dolores aliquid sed consectetur, iste sit a reiciendis laboriosam maiores.</Typography>
+        </div>
+    );
+};
+
+export default MuiTypography;
